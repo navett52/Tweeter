96764a51935049a75d3651f856e26c8ea15142f0
Justin Meyer <meyer3js@mail.uc.edu>
2017-04-08 20:43:57 -0400

    -Removed outdated TweetSharp api -Added TweetInvi api

---
+++ App_Code/TweetReader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+/// <summary>
+/// Summary description for TweetReader
+/// </summary>
+public class TweetReader
+{
+    string hashTag; // The hashtag to read results from
+
+    public TweetReader(string hashTag)
+    {
+        this.hashTag = hashTag;
+    }
+
+    
+}
