40d170ffd58990093fb6a200c1835933d23a96e3
SirChibi <reilmajb@mail.uc.edu>
2017-04-09 01:24:24 -0400

    Made getUsersFriends class
    Class where you enter a username and get the first 250 of the people
    that they have followed.

---
+++ App_Code/GetUsersFriends.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Tweetinvi;
+
+/// <summary>
+/// Summary description for GetUsersFriends
+/// </summary>
+public class GetUsersFriends
+{
+    public List<string> getUserFriends(string userName)
+    {
+        List<string> friendList = new List<string>();
+        var user = User.GetUserFromScreenName(userName);
+
+        // Get the first 250 friends of the user
+        var friends = User.GetFriends(user);
+        return friendList;
+    }
+}
