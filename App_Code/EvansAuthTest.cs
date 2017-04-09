aaf72696b3d1e0d2d96374fd260ad14a9a1546cb
Evan Tellep <evantellep@yahoo.com>
2017-04-09 04:49:50 -0400

    Stuff

--- App_Code/EvansAuthTest.cs
+++ App_Code/EvansAuthTest.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using Tweetinvi;
 using Tweetinvi.Models;
+using System.Data;
+using ASPSnippets.TwitterAPI;
 
 /// <summary>
 /// Summary description for EvansAuthTest
@@ -16,57 +18,11 @@ public class EvansAuthTest
     string accessToken = "850829466905530369-wC0Zsz4R9nme5gHupsUSllajIUEtOca";
     string accessTokenSecret = "u4LUvF9WzzwxyUAGb8qaUysdj4g9Q7DiqfOQOPLxCJs49";
 
+    ITwitterCredentials credentials = Auth.SetUserCredentials("rjDDIP0kaevQqXlMFoEVsQyMP", "bYUJv72a9i8c1HzMpjq98xcDduAufw77dsceN9YBchvk4Y9WS4", "850829466905530369-wC0Zsz4R9nme5gHupsUSllajIUEtOca", "u4LUvF9WzzwxyUAGb8qaUysdj4g9Q7DiqfOQOPLxCJs49");
+
     public EvansAuthTest()
     {
-        //
-        // TODO: Add constructor logic here
-        //
-    }
-
-    public List<String> getTweets(string hashtag)
-    {
-        AuthFlow_CreateFromRedirectedCallbackURL_StepByStep(consumerKey, consumerSecret);
-        return null;
-    }
-
-    // Get credentials with callbackURL system
-    public static ITwitterCredentials AuthFlow_CreateFromRedirectedCallbackURL_StepByStep(string consumerKey, string consumerSecret)
-    {
-        var applicationCredentials = new ConsumerCredentials(consumerKey, consumerSecret);
-        var authenticationContext = AuthFlow.InitAuthentication(applicationCredentials, "http://localhost:64430/Default.aspx");
-        Console.WriteLine("Go on : {0}", authenticationContext);
-        Console.WriteLine("When redirected to your website copy and paste the URL: ");
-
-        // Enter a value like: https://tweeetinvi.codeplex.com?oauth_token={tokenValue}&oauth_verifier={verifierValue}
-
-        var callbackURL = Console.ReadLine();
-
-        // Here we provide the entire URL where the user has been redirected
-        var newCredentials = AuthFlow.CreateCredentialsFromCallbackURL(callbackURL, authenticationContext);
-        Console.WriteLine("Access Token = {0}", newCredentials.AccessToken);
-        Console.WriteLine("Access Token Secret = {0}", newCredentials.AccessTokenSecret);
-
-        return newCredentials;
-    }
-
-    public static ITwitterCredentials AuthFlow_CreateFromRedirectedVerifierCode_StepByStep(string consumerKey, string consumerSecret)
-    {
-        var applicationCredentials = new ConsumerCredentials(consumerKey, consumerSecret);
-        var authenticationContext = AuthFlow.InitAuthentication(applicationCredentials, "https://tweetinvi.codeplex.com");
-        Console.WriteLine("Go on : {0}", authenticationContext);
-        Console.WriteLine("When redirected to your website copy and paste the value of the oauth_verifier : ");
-
-        // For the following redirection https://tweetinvi.codeplex.com?oauth_token=UR3eTEwDXFNhkMnjqz0oFbRauvAm4YhnF67KE6hO8Q&oauth_verifier=woXaKhpDtX6vhDVPl7TU6955qdQeH3cgz6xDvRZRA4A
-        // Enter the value : woXaKhpDtX6vhDVPl7TU6955qdQeH3cgz6xDvRZRA4A
-
-        var verifierCode = Console.ReadLine();
-
-        // Here we only provide the verifier code
-        var newCredentials = AuthFlow.CreateCredentialsFromVerifierCode(verifierCode, authenticationContext);
-        Console.WriteLine("Access Token = {0}", newCredentials.AccessToken);
-        Console.WriteLine("Access Token Secret = {0}", newCredentials.AccessTokenSecret);
-
-        return newCredentials;
+        
     }
 
 }
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Twitter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Twitter")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a6d36e76-e35e-4fcb-9ab4-ed22a2639e49")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
